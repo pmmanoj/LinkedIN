d672479ff8508ac04d2de5e67236e55cd3c5b515
pmmanoj <pmanoj@ibdsys.com>
2017-12-12 23:18:42 -0600

    Added full SBT directories

---
+++ src/main/scala/example/Main.scala
@@ -0,0 +1,11 @@
+package example
+
+/**
+  * Created by sanjivsingh on 12/12/17.
+  */
+object Main extends App {
+  val ages = Seq(42, 75, 29, 64)
+  println( s" The oldest person is ${ages.max}")
+
+}
+
